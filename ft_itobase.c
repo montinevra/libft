c9b8b68b86470f2f955dc1cfd437e82fbde9151a
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-01-11 23:11:45 -0800

    itobase now always mallocs a new string

--- ft_itobase.c
+++ ft_itobase.c
@@ -6,7 +6,7 @@
 /*   By: pvan-erp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/10 15:08:30 by pvan-erp          #+#    #+#             */
-/*   Updated: 2017/01/11 21:49:03 by pvan-erp         ###   ########.fr       */
+/*   Updated: 2017/01/11 23:10:57 by pvan-erp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ static unsigned int	get_digits_base(unsigned long n, const unsigned int radix)
 
 	digits = 0;
 	if (n == 0)
+		return (1);
+	if (radix <= 1)
 		return (1);
 	while (n)
 	{
@@ -38,17 +40,20 @@ char				*ft_itobase(unsigned long n, const unsigned int radix,
 	char			*fresh;
 	unsigned int	digits;
 
-	if (n == 0)
-		return ("0");
-	if (radix < 2 || radix > ft_strlen(base))
-		return ("undefined");
 	digits = get_digits_base(n, radix);
 	if (!(fresh = ft_strnew(digits)))
 		return (NULL);
-	while (n)
+	if (n == 0)
+		fresh[0] = '0';
+	else
 	{
-		fresh[--digits] = base[(n % radix)];
-		n /= radix;
+		if (radix < 2 || radix > ft_strlen(base))
+			return ("undefined");
+		while (n)
+		{
+			fresh[--digits] = base[(n % radix)];
+			n /= radix;
+		}
 	}
 	return (fresh);
 }
