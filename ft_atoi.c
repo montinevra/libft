6dfa1c19ab65e8daee594ffd751ccc55d218bc92
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-05-02 00:21:32 -0700

    atoi now checks for non-null string

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,19 @@
 /*   By: pvan-erp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/25 20:04:21 by pvan-erp          #+#    #+#             */
-/*   Updated: 2017/03/16 16:21:12 by pvan-erp         ###   ########.fr       */
+/*   Updated: 2017/05/02 00:20:54 by pvan-erp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	use_base(const char *str)
+{
+	if (ft_tolower(str[1]) == 'x')
+		return (ft_atoi_base(&str[2], 16));
+	else
+		return (ft_atoi_base(&str[1], 8));
+}
 
 int			ft_atoi(const char *str)
 {
@@ -18,25 +26,23 @@ int			ft_atoi(const char *str)
 	int i;
 	int sign;
 
-	i = 0;
 	nb = 0;
 	sign = 1;
-	while (ft_isspace(str[i]))
-		i++;
-	if (str[i] == '0')
+	if (str)
 	{
-		if (ft_tolower(str[i + 1]) == 'x')
-			return (ft_atoi_base(&str[i + 2], 16));
-		else
-			return (ft_atoi_base(&str[i], 8));
-	}
-	else
-	{
-		if (str[i] == '-')
-			sign = -1;
-		if (str[i] == '+' || str[i] == '-')
+		i = 0;
+		while (ft_isspace(str[i]))
 			i++;
-		nb = ft_atoi_base(&str[i], 10);
+		if (str[i] == '0')
+			return (use_base(&str[i]));
+		else
+		{
+			if (str[i] == '-')
+				sign = -1;
+			if (str[i] == '+' || str[i] == '-')
+				i++;
+			nb = ft_atoi_base(&str[i], 10);
+		}
 	}
 	return (nb * sign);
 }
